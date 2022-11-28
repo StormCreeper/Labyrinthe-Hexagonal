a31988106c5a9ec3f70dbf89f3e7fafd8c9b060d
Telo Philippe <telo.philippe@telecom-paris.fr>
2022-11-28 14:16:37 +0100

    Ajouté les fichiers du tp précédent

---
+++ src/graph/ShortestPaths.java
@@ -0,0 +1,23 @@
+package graph;
+
+/**
+ * Interface définissant le chemin le plus court d'un sommet à l'origine, sous forme de liste chaînée.
+ * @author Telo PHILIPPE
+ *
+ */
+public interface ShortestPaths {
+	
+	/**
+	 * Permet de récupérer le prédécesseur d'un sommet dans la chaîne définissant le chemin.
+	 * @param vertex le sommet dont on veut le prédécesseur.
+	 * @return le prédecesseur du sommet.
+	 */
+	public Vertex getPrevious(Vertex vertex);
+	
+	/**
+	 * Permet de modifier le prédécesseur d'un sommet.
+	 * @param vertex le sommet à modifier.
+	 * @param previous son prédécesseur.
+	 */
+	public void setPrevious(Vertex vertex, Vertex previous);
+}
